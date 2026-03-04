7eceebef022f78167ea0552fd9051e4d140bd98b
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-03-04 22:58:04 +0900

    update: 잘못된 구문 제거

--- src/common/middleware/correlation.middleware.ts
+++ src/common/middleware/correlation.middleware.ts
@@ -1,4 +1,4 @@
-import git{ Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { AlsService } from '../als/als.service';
